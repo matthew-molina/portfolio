db0b65295824ae5ca3678e9d360c56c7a935b233
STD <mattiyahu11@gmail.com>
2018-07-07 01:18:39 +0800

    Added new keyword

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    STD
                 </div>
 
                 <div class="links">
